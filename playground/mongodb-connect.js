53f429dff13d07ef50d13495e36f0381a869b107
Kieran Chadwick <kieran.chadwick@therobertstreethub.com>
2018-02-02 17:23:11 +0000

    extracting timestamp from objectID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,9 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const uri = `mongodb://admin:o4OHqmlXD30bcWFz@playground-shard-00-00-kn1ug.mongodb.net:27017,playground-shard-00-01-kn1ug.mongodb.net:27017,playground-shard-00-02-kn1ug.mongodb.net:27017/todoapp?ssl=true&replicaSet=Playground-shard-0&authSource=admin`
+
+/* const obj = new ObjectID();
+console.log(obj); */
 
 MongoClient.connect(uri, (err, client) => {
     if (err) {
@@ -31,7 +34,9 @@ MongoClient.connect(uri, (err, client) => {
             return console.log('Unable to insert user', err);
         }
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
+        const docCreated = result.ops[0];
+        console.log(JSON.stringify(docCreated, undefined, 2));
+        console.log(docCreated._id.getTimestamp());
     });
 
     client.close();
