409ec1863c839419c7cdc5b3fc24a1c065a6254c
Kieran Chadwick <kieran.chadwick@therobertstreethub.com>
2018-02-15 11:07:55 +0000

    interchange db across different environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 // use promises
 mongoose.Promise = global.Promise;
-const uri = `mongodb://admin:o4OHqmlXD30bcWFz@playground-shard-00-00-kn1ug.mongodb.net:27017,playground-shard-00-01-kn1ug.mongodb.net:27017,playground-shard-00-02-kn1ug.mongodb.net:27017/todoapp?ssl=true&replicaSet=Playground-shard-0&authSource=admin`
+const uri = `mongodb://admin:o4OHqmlXD30bcWFz@playground-shard-00-00-kn1ug.mongodb.net:27017,playground-shard-00-01-kn1ug.mongodb.net:27017,playground-shard-00-02-kn1ug.mongodb.net:27017/${process.env.MONGODB_DB}?ssl=true&replicaSet=Playground-shard-0&authSource=admin`;
+
+console.log(uri);
 
 // connect
 mongoose.connect(uri);
